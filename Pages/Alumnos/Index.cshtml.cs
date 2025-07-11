412b5809f1f94d52b7507499d69bc2a8b2cc9fd1
Lorenzo <lorenzooliverio12@gmail.com>
2025-07-11 14:31:29 -0300

    Solucion errores de create.html.cs de alumno y tablas de alumno y carrera

--- Pages/Alumnos/Index.cshtml.cs
+++ Pages/Alumnos/Index.cshtml.cs
@@ -1,17 +1,28 @@
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademico.AccesoADatos;
+using SistemaAcademico.AccesoDatos;
 using SistemaAcademico.Data;
 using SistemaAcademico.Models;
+using SistemaAcademico.Repositorio;
+using SistemaAcademico.Services;
 
 namespace SistemaAcademico.Pages.Alumnos
 {
     public class IndexModel : PageModel
     {
-        public List<Alumno> Alumnos { get; set; }
+		private readonly ServicioAlumno servicio;
+		public IndexModel()
+		{
+			IAccesoDatos<Alumno> acceso = new AccesoDatosJson<Alumno>("Alumno");
+			IRepositorio<Alumno> repo = new RepositorioCrudJson<Alumno>(acceso);
+			servicio = new ServicioAlumno(repo);
+		}
+		public List<Alumno> Alumnos { get; set; }
         public void OnGet()
         {
-            Alumnos = DatosCompartidos.Alumnos;
+            Alumnos = servicio.ObtenerTodos();
         }
     }
 }
--- Pages/Carreras/Index.cshtml.cs
+++ Pages/Carreras/Index.cshtml.cs
@@ -1,18 +1,28 @@
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SistemaAcademico.AccesoADatos;
+using SistemaAcademico.AccesoDatos;
 using SistemaAcademico.Data;
 using SistemaAcademico.Models;
+using SistemaAcademico.Repositorio;
 using SistemaAcademico.Services;
 
 namespace SistemaAcademico.Pages.Carreras
 {
     public class IndexModel : PageModel
     {
-        public List<Carrera> Carreras { get; set; }
+		private readonly ServicesCareer servicio;
+		public IndexModel()
+		{
+			IAccesoDatos<Carrera> acceso = new AccesoDatosJson<Carrera>("Carrera");
+			IRepositorio<Carrera> repo = new RepositorioCrudJson<Carrera>(acceso);
+			servicio = new ServicesCareer(repo);
+		}
+		public List<Carrera> Carreras { get; set; }
         public void OnGet()
         {
-            Carreras = ServicesCareer.ObtenerCarreras();
+            Carreras = servicio.ObtenerTodos();
         }
     }
 }
